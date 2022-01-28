7d2e60ad7bfe5814f3e12d5ecf3d632d365a1bd9
Jaren Cudilla <jarencudilla@gmail.com>
2022-01-28 13:55:53 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
+require("dotenv").config();
+var md5 = require('md5');
 const axios = require('axios');
-const URL =  'https://gateway.marvel.com:';
-const port = 443;
-const apiKey = 'd73076c29232a459b9583e1f5e511316';
-const charURL = '/v1/public/characters?'
+const URL =  "https://gateway.marvel.com:";
+const PORT = process.env.PORT || 443
+const PUBLIC_KEY = process.env.MARVEL_KEY;
+const PRIVATE_KEY = process.env.ANOTHER_MARVEL_KEY;
+// const API_KEY = PRIVATE_KEY + PUBLIC_KEY;
+const charURL = "/v1/public/characters?";
+const TS = new Date().getTime();
 
+console.log(TS);
+console.log(PUBLIC_KEY);
 
+var stringToHash = TS + PRIVATE_KEY + PUBLIC_KEY;
+var hash = md5(stringToHash);
+
+var data = "wolverine";
 
 
 axios
-  .get(URL + port + charURL + 'apikey=' + apiKey)
+  .get(URL + PORT + charURL + 'ts=' + TS +'&apikey=' + PUBLIC_KEY + '&hash=' + hash )
   .then(response => {
     console.log(response);
   })
   .catch(error => {
     console.log(error);
   });
+
